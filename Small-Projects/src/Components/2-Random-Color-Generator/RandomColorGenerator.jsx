afa9d49b419bb9f078a672a56c8ca68179db2920
Shubham <shubhampatil1356@gmail.com>
2025-01-24 00:54:10 +0530

    Impleamented hex and hsl color generator based on users choice.

--- Small-Projects/src/Components/2-Random-Color-Generator/RandomColorGenerator.jsx
+++ Small-Projects/src/Components/2-Random-Color-Generator/RandomColorGenerator.jsx
@@ -1,30 +1,86 @@
 import React, { useState } from "react";
 
-
 function RandomColorGenerator() {
-
     const [randomColor, setRandomColor] = useState("Click On Generate Button");
+    const [colorFormat, setColorFormat] = useState("rgb");
 
-    function handleGenerateRgbColor() {
-        const red = Math.floor(Math.random() * 256);
-        const green = Math.floor(Math.random() * 256);
-        const blue = Math.floor(Math.random() * 256);
+    function generateRgbColor() {
+        return ({
+            red: Math.floor(Math.random() * 256),
+            green: Math.floor(Math.random() * 256),
+            blue: Math.floor(Math.random() * 256),
+        });
+    }
 
-        const rgbColor = `RGB(${red}, ${green}, ${blue})`;
+    function generateHexColor() {
+        const rgb = generateRgbColor();
 
-        setRandomColor(rgbColor);
+        const hex = {
+            red: rgb.red.toString(16).padStart(2, "0"),
+            green: rgb.green.toString(16).padStart(2, "0"),
+            blue: rgb.blue.toString(16).padStart(2, "0"),
+        }
+
+        return (`#${hex.red + hex.green + hex.blue}`);
+    }
+
+    function generateHslColor() {
+        const hsl = {
+            hue: Math.floor(Math.random() * 360),
+            saturation: Math.floor(Math.random() * 101),
+            lightness: Math.floor(Math.random() * 101),
+        };
+
+        return (`hsl(${hsl.hue}, ${hsl.saturation}%, ${hsl.lightness}%)`);
+    }
+
+    function handleGenerateColor() {
+        let color;
+
+        switch (colorFormat) {
+            case "rgb":
+                const rgb = generateRgbColor();
+                color = `rgb(${rgb.red}, ${rgb.green}, ${rgb.blue})`;
+                break;
+            case "hex":
+                color = generateHexColor();
+                break;
+            case "hsl":
+                color = generateHslColor();
+                break;
+            default:
+                console.log("Invalid Color Format.");
+        }
+
+        setRandomColor(color);
+    }
+
+    function handleColorFormatChange(event) {
+        setColorFormat(event.target.value);
     }
 
     return (
-        <div className="wrapper">
+        <div className='wrapper'>
             <h2>Random Color Generator</h2>
-            <button onClick={handleGenerateRgbColor}>Generator Rgb Color</button>
-            <div className="color" style={{ backgroundColor: randomColor }}>
+            <div className='controls'>
+                <label htmlFor='color-format'>Chose a Color Format: </label>
+                <select
+                    value={colorFormat}
+                    onChange={handleColorFormatChange}
+                    name='color-format'
+                    id='color-format'>
+                    <option value='rgb'>Rgb</option>
+                    <option value='hex'>Hex</option>
+                    <option value='hsl'>Hsl</option>
+                </select>
+                <button onClick={handleGenerateColor}>Generate Color</button>
+            </div>
+
+            <div className='color' style={{ backgroundColor: randomColor }}>
                 <p>{randomColor}</p>
             </div>
         </div>
-    )
+    );
 }
-
 
 export default RandomColorGenerator;
