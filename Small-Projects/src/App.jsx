b15720af6c10ace4a600e4ebdb938e0013f47d7f
Shubham <shubhampatil1356@gmail.com>
2025-01-25 01:35:35 +0530

    Added a Star Rating Component and implemented it's functionality.

--- Small-Projects/src/App.jsx
+++ Small-Projects/src/App.jsx
@@ -1,13 +1,16 @@
 import Accordion from "./Components/1-Accordion/Accordion.jsx";
 import RandomColorGenerator from "./Components/2-Random-Color-Generator/RandomColorGenerator.jsx";
+import StarRating from "./Components/3-Star-Rating/StarRating.jsx";
 
 function App() {
 
   return (
     <>
-      <Accordion />
+      {/* <Accordion />
       <hr />
       <RandomColorGenerator />
+      <hr /> */}
+      <StarRating />
     </>
   )
 }
---
+++ Small-Projects/src/Components/3-Star-Rating/StarRating.jsx
@@ -0,0 +1,48 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import style from "./StarRating.module.css";
+import React, { useState } from "react";
+
+function StarRating({ noOfStars = 5 }) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex) {
+        setRating(getCurrentIndex);
+    }
+
+    function handleMouseHover(getCurrentIndex) {
+        setHover(getCurrentIndex);
+    }
+
+    function handleMouseLeave() {
+        setHover(rating);
+    }
+
+    return (
+        <div className='container'>
+            <p>Project 3</p>
+            <h2>Star Ratings</h2>
+
+            <div>
+                {[...Array(noOfStars)].map((_, index) => {
+                    index += 1; // index starts from 1
+
+                    return (
+                        <i
+                            className={`fa-star 
+                                ${index <= (hover || rating)
+                                    ? style.active + " fa-solid"
+                                    : style.inactive + " fa-regular"
+                                }`}
+                            key={index}
+                            onClick={() => handleClick(index)}
+                            onMouseMove={() => handleMouseHover(index)}
+                            onMouseLeave={handleMouseLeave}></i>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default StarRating;
